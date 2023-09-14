fcf79fb517994a64da259b8c7f2e0b36f54b989b
BrianEtemesi <etemecci@gmail.com>
2023-09-14 14:08:20 +0000

    configure django with mysql credentials

--- lawris/settings.py
+++ lawris/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'lawris.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lawris_db',
+        'USER': 'lawris_admin',
+        'PASSWORD': 'sirwal',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
