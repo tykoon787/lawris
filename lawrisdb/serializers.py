17f3b3bb545ec8023ff83496fbf4b655c20f0082
BrianEtemesi <etemecci@gmail.com>
2023-09-16 14:06:06 +0000

    configure serializers for all models

--- lawrisdb/serializers.py
+++ lawrisdb/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from . models import CustomUser
+from . models import *
 from django.contrib.auth.models import Permission
 
 class CustomUserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +12,62 @@ class CustomUserSerializer(serializers.HyperlinkedModelSerializer):
 class PermissionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Permission
+
+
+class ActivityLogSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ActivityLog
+        fields = '__all__'
+
+
+class CaseRulingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = CaseRuling
+        fields = '__all__'
+
+
+class ClientSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Client
+        fields = '__all__'
+
+
+class DocumentTemplateSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = DocumentTemplate
+        fields = '__all__'
+
+
+class DocumentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Document
+        fields = '__all__'
+
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+
+class InvoiceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Invoice
+        fields = '__all__'
+
+
+class LawFirmSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = LawFirm
+        fields = '__all__'
+
+
+class LegalCaseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = LegalCase
+        fields = '__all__'
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Task
+        fields = '__all__'
