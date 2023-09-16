411496bea79b0116944ad1d238da3de633dc074a
BrianEtemesi <etemecci@gmail.com>
2023-09-16 13:36:23 +0000

    add pending models and run migrations

--- lawrisdb/models.py
+++ lawrisdb/models.py
@@ -38,3 +38,98 @@ class CustomUser(AbstractUser, BaseModel):
 
     def __str__(self):
         return self.username
+
+
+
+# 2. Client
+class Client(BaseModel):
+    name = models.CharField(max_length=100)
+    contact_details = models.TextField(blank=True, null=True)
+    case_history = models.TextField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+# 3. Law Firm
+class LawFirm(BaseModel):
+    name = models.CharField(max_length=100)
+    lawyers = models.ManyToManyField(CustomUser, related_name='law_firms')  # Many-to-Many relationship with lawyers
+
+    def __str__(self):
+        return self.name
+
+# 4. Case
+class LegalCase(BaseModel):
+    case_number = models.CharField(max_length=50)
+    case_type = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=50)
+    clients = models.ManyToManyField(Client, related_name='cases')  # Many-to-Many relationship with clients
+    lawyers = models.ManyToManyField(CustomUser, related_name='cases')  # Many-to-Many relationship with lawyers
+
+    def __str__(self):
+        return self.case_number
+
+# 5. Document
+class Document(BaseModel):
+    document_type = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date = models.DateField()
+    case = models.ForeignKey(LegalCase, on_delete=models.CASCADE, related_name='documents', blank=True, null=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='documents', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+# 6. Event/Calendar
+class Event(BaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=200, blank=True, null=True)
+    attendees = models.ManyToManyField(CustomUser, related_name='events')  # Many-to-Many relationship with users
+
+    def __str__(self):
+        return self.title
+
+# 7. Task/To-Do
+class Task(BaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateField()
+    priority = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+# 8. Case Rulings/Precedents
+class CaseRuling(BaseModel):
+    case_number = models.CharField(max_length=50)
+    court = models.CharField(max_length=100)
+    judges = models.TextField()
+    parties_involved = models.TextField()
+    legal_implications = models.TextField()
+
+    def __str__(self):
+        return self.case_number
+
+# 10. Document Templates
+class DocumentTemplate(BaseModel):
+    template_type = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+# 13. Notifications/Activity Feed
+class ActivityLog(BaseModel):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    activity_type = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    details = models.TextField()
+
+# 14. Billing/Invoices
+class Invoice(BaseModel):
+    # Define fields for invoices and payment records as needed
+    amount = models.CharField(max_length=100)
