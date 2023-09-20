563cc493fe5c615f09d6141a0408938a5c873965
BrianEtemesi <etemecci@gmail.com>
2023-09-20 14:56:53 +0000

    define models

--- acct/models.py
+++ acct/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from lawris.basemodel import BaseModel
 
-# Create your models here.
+class Invoice(BaseModel):
+    # Define fields for invoices and payment records as needed
+    amount = models.CharField(max_length=100)
