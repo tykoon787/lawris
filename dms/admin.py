f279728b11f11e7de5e0f6146765e0c10549c696
BrianEtemesi <etemecci@gmail.com>
2023-09-20 12:28:47 +0000

    register dms models to admin

--- dms/admin.py
+++ dms/admin.py
@@ -1,3 +1,15 @@
+"""
+In order for our models to be visible on the admin page, we need
+to register them by opening the admin.py file (inside the app folder)
+and use the following command to register the models.
+"""
+
 from django.contrib import admin
+from dms.models import Document, DocumentTemplate, CaseRuling
+
 
 # Register your models here.
+
+admin.site.register(Document)
+admin.site.register(CaseRuling)
+admin.site.register(DocumentTemplate)
