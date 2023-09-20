51ce245ad076f03fdc88147780d67d7959f22e53
BrianEtemesi <etemecci@gmail.com>
2023-09-20 15:24:57 +0000

    add client name field

--- acct/models.py
+++ acct/models.py
@@ -3,4 +3,5 @@ from lawris.basemodel import BaseModel
 
 class Invoice(BaseModel):
     # Define fields for invoices and payment records as needed
+    client = models.TextField(blank=True, null=True)
     amount = models.CharField(max_length=100)
