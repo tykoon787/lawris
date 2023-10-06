ded082acf551ea9ca43d905fef4b59bfde6acb0e
tykoon787 <tykoon787@protonmail.com>
2023-10-06 17:19:42 +0300

    Created a script to generate template files for the db

--- lawris-app/lawris_django/dms/scripts/create_templates.py
+++ lawris-app/lawris_django/dms/scripts/create_templates.py
@@ -56,6 +56,7 @@ def get_thumbnail(thumbnail_path: str):
     """
     Retrieves the thumbnail img
     """
+    # NOTE: This method leads to a Suspicious File Operation Error
     # image_temp_file = NamedTemporaryFile(delete=True)
 
     # in_memory_image = open(thumbnail_path, 'rb')
