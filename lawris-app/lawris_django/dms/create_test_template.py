7a3e60183183c7762b3d542fe72d5d3565b4c0c1
tykoon787 <tykoon787@protonmail.com>
2023-11-23 14:08:03 +0300

    Defined function for creating test templates

--- lawris-app/lawris_django/dms/create_test_template.py
+++ lawris-app/lawris_django/dms/create_test_template.py
@@ -1,200 +1,214 @@
+# This file is used to create test templates
 from .models import Template
 
+# NOTE: Replace this with the template_file_docx. This is only a placeholder
+file = "test/path"
 
 form_78_data = {
     'type': 'Petition',
-    'title': 'Grant of probate',
+    'title': 'Grant of Probate 1',
     'category_of_law': 'civil',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_79_data = {
-    'type': 'Affidavit of Service',
-    'title': 'Grant of probate',
+    'type': 'Affidavit',
+    'title': 'Affidavit of Service',
     'category_of_law': 'Criminal',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_80_data = {
-    'type': 'Amended Plaint',
-    'title': 'Grant of probate',
-    'category_of_law': 'Civil',
+    'type': 'Petition',
+    'title': 'Amended Plaint',
+    'category_of_law': 'Criminal',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_80_data = {
-    'type': 'Grant of Probate Intestate',
-    'title': 'Grant of probate',
+    'type': 'Petition',
+    'title': 'Grant of probate Intestate',
     'category_of_law': 'Civil',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_81_data = {
-    'type': 'Affidavit of Service',
-    'title': 'Grant of probate',
+    'type': 'Affidavit',
+    'title': 'Affidavit of Service 2',
     'category_of_law': 'Civil',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_82_data = {
-    'type': 'Plaint Fast Track',
-    'title': 'Grant of probate',
+    'type': 'Petition',
+    'title': 'Grant of probate for Administrtor',
     'category_of_law': 'Civil',
-    'division_of_law': 'family',
+    'division_of_law': 'land',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_83_data = {
-    'type': 'Amended Plaint',
-    'title': 'Grant of probate',
+    'type': 'Affidavit',
+    'title': 'Affidavit of service 3',
     'category_of_law': 'Criminal',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_84_data = {
-    'type': 'Amended Plaint',
-    'title': 'Grant of probate',
+    'type': 'petition',
+    'title': 'Grant of probate 4',
     'category_of_law': 'Criminal',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_85_data = {
-    'type': 'Grant of Probate Intestate',
+    'type': 'Petition',
     'title': 'Grant of probate',
     'category_of_law': 'Civil',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_86_data = {
-    'type': 'Amended Plaint',
+    'type': 'Affidavit',
     'title': 'Grant of probate',
     'category_of_law': 'Crimial',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_87_data = {
-    'type': 'Grant of Probate Intestate',
-    'title': 'Grant of probate',
+    'type': 'Petition',
+    'title': 'Grant of probate Intestate 3',
     'category_of_law': 'Civil',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_88_data = {
-    'type': 'Plaint Fast Track',
+    'type': 'petition',
     'title': 'Grant of probate',
     'category_of_law': 'Criminal',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
 form_89_data = {
-    'type': 'Amended Plaint',
-    'title': 'Grant of probate',
+    'type': 'Affidavit',
+    'title': 'Grant of probate as an Affidavit',
     'category_of_law': 'Criminal',
     'division_of_law': 'family',
     'sub_division': 'succession',
     'template_file_docx': file,
-    'pdf_preview_file' : "/mnt/preview",
-    'form_fields' : {
+    'pdf_preview_file': "/mnt/preview",
+    'form_fields': {
         'field1': 'Field 1',
         'field2': 'Field 2',
     }
 }
 
+
+def template_creater(template_data: dict):
+    """
+    This method handles the creation of the template by calling
+    the `create_template` from the `Template` Model
+    """
+    created_template = Template.create_template(**template_data)
+    print(f"Created {created_template} Template")
+    return created_template
+
+
 def main():
-    form_78 = Template.create_template(**form_78_data)
-    form_79 = Template.create_template(**form_79_data)
-    form_80 = Template.create_template(**form_80_data)
-    form_81 = Template.create_template(**form_81_data)
-    form_82 = Template.create_template(**form_82_data)
-    form_83 = Template.create_template(**form_83_data)
-    form_84 = Template.create_template(**form_84_data)
-    form_85 = Template.create_template(**form_85_data)
-    form_86 = Template.create_template(**form_86_data)
-    form_87 = Template.create_template(**form_87_data)
-    form_88 = Template.create_template(**form_88_data)
-    form_89 = Template.create_template(**form_89_data)
+
+    Templates = [
+        form_78_data, form_79_data, form_80_data, form_81_data, form_82_data,
+        form_83_data, form_84_data, form_85_data, form_86_data, form_87_data,
+        form_88_data, form_89_data
+    ]
+    for template_data in Templates:
+        template_creater(template_data)
+
+    print(f"Finished Creating {len(Templates)} Templates")
+
+
 def run():
     main()
