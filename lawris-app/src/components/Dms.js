3251b7316d1fc2c66aef9ef1990fe43c8b41a101
Wanton Raven <josephngalu96@gmail.com>
2023-12-09 11:21:42 +0000

    Added some blurness

--- lawris-app/src/components/Dms.js
+++ lawris-app/src/components/Dms.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import $ from 'jquery';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import User from '../Assets/non-litigant.jpg';
+// import User from '../Assets/non-litigant.jpg';
 import Search from '../Assets/search.png';
 import Account from '../Assets/user.png';
 import Logout from '../Assets/exit.png';
@@ -18,7 +18,7 @@ import addFile from '../static/icons/dms/icons/new-file.png';
 import archive from '../static/icons/dms/icons/archive.png';
 import upload from '../static/icons/dms/icons/upload.png';
 import pdf from '../static/icons/dms/icons/pdf.png';
-import apps from '../static/icons/dms/icons/apps.svg';
+// import apps from '../static/icons/dms/icons/apps.svg';
 import logo from '../Assets/transparentLawrisLogo.png';
 
 //importing app icons
@@ -480,7 +480,7 @@ const Dms = () => {
             {showUpload && 
             <form class="file-upload-form mx-auto" style={{ zIndex: 9999 }}>
             <label for="file" class="file-upload-label">
-                <div class="file-upload-design">
+                <div class="file-upload-design glass">
                 <svg viewBox="0 0 640 512" height="1em">
                                 <path
                                 d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"
@@ -500,7 +500,7 @@ const Dms = () => {
             {showConvert && 
             <form class="file-upload-form mx-auto" style={{ zIndex: 9999 }}>
             <label for="file" class="file-upload-label">
-                <div class="file-upload-design">
+                <div class="file-upload-design glass">
                 <i class="bi bi-filetype-pdf" style={{fontSize: '40px'}} ></i>
                 <p>Drag and Drop</p>
                 <p>or</p>
@@ -513,7 +513,7 @@ const Dms = () => {
             {isFileUploaded && 
             <form class="file-upload-form mx-auto" style={{ zIndex: 9999 }}>
             <label for="file" class="file-upload-label">
-                <div class="file-upload-design">
+                <div class="file-upload-design glass">
                 <div class={isUpload? 'installer' : 'installer2'}>
 	                <label for="progressLinux"><input id="progressLinux" type="radio" /><span></span></label>
                 </div>
@@ -529,7 +529,7 @@ const Dms = () => {
             <form class="file-upload-form mx-auto" style={{ zIndex: 9999 }}>
                 
                 <div class="file-upload-label">
-            <div class="file-upload-design2">
+            <div class="file-upload-design2 glass">
             <label className='my-auto' role="button">
             
                 <form action="/upload" method="post" enctype="multipart/form-data">
--- lawris-app/src/components/Notifications.js
+++ lawris-app/src/components/Notifications.js
@@ -13,13 +13,13 @@ function Notifications({ time, fileName }) {
     const [show, setShow] = useState(true);
  return (
    <ToastContainer className='myToastContainer'>
-       <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
-       <Toast.Header>
+       <Toast className="glass" onClose={() => setShow(false)} show={show} delay={3000} autohide>
+       <Toast.Header >
            <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
            <strong className="me-auto">Notification</strong>
            <small>{getTimeDifference(time)}</small>
        </Toast.Header>
-       <Toast.Body>{fileName} uploaded successfully.</Toast.Body>
+       <Toast.Body >{fileName} uploaded successfully.</Toast.Body>
        </Toast>
    </ToastContainer>
  );
