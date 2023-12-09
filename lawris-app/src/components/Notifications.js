6b42b9c4e2f87b63cfd3a9a51f170204a5530626
Wanton Raven <josephngalu96@gmail.com>
2023-12-09 11:34:33 +0000

    removed glass effect on notifications

--- lawris-app/src/components/Notifications.js
+++ lawris-app/src/components/Notifications.js
@@ -13,7 +13,7 @@ function Notifications({ time, fileName }) {
     const [show, setShow] = useState(true);
  return (
    <ToastContainer className='myToastContainer'>
-       <Toast className="glass" onClose={() => setShow(false)} show={show} delay={3000} autohide>
+       <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
        <Toast.Header >
            <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
            <strong className="me-auto">Notification</strong>
