b9e6710b768fc117431afc79a0071e49eee535d0
tykoon787 <tykoon787@protonmail.com>
2024-01-08 17:25:40 +0300

    Changed to local folder

--- lawris-app/lawris_django/lawris_django/settings.py
+++ lawris-app/lawris_django/lawris_django/settings.py
@@ -59,7 +59,7 @@ from firebase_admin import credentials
 
 # Path to your service account key JSON file
 # cred = credentials.Certificate("/workspace/lawris/lawris-app/lawris_django/lawris_django/authentication-2f5cc-firebase-adminsdk-f0dy2-c01b29d1e6.json")
-cred = credentials.Certificate("lawris_django/firebase/authentication-2f5cc-firebase-adminsdk-f0dy2-c01b29d1e6.json")
+cred = credentials.Certificate("/home/tykoon787/projects/firebase_creds/authentication-2f5cc-firebase-adminsdk-f0dy2-c01b29d1e6.json")
 
 # Initialize Firebase Admin SDK
 firebase_admin.initialize_app(cred)
